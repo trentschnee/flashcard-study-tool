30b97485f21104ca7edd882067e3049ac700571f
trentschnee <tschneweis95@outlook.com>
2020-04-06 15:45:01 -0500

    chore: Add dependencies feat: Add Tabs

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator  } from '@react-navigation/stack';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+<Tabs.Navigator>
+  <Tabs.Screen>
+  <Tabs.Screen name="Add Entry" component={<Text>Hello</Text>} />
+  </Tabs.Screen>
+</Tabs.Navigator>
+      </NavigationContainer>
     </View>
+
   );
 }
 
