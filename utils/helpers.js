3521a52899ba3e9c9eb16dd5b90f2b64fef1589e
trentschnee <tschneweis95@outlook.com>
2020-04-14 15:29:34 -0500

    feat: Add LocalNotifications

---
+++ utils/helpers.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { View, Text, AsyncStorage } from 'react-native'
+import { Notifications, Permissions } from 'expo'
+const FLASHCARDS_NOTIFICATION_DB = 'flashcards:notification'
+function createLocalNotification() {
+    // Return an object that says don't forget to study
+    return {
+        title: `S+T+U+D+Y = STUDY`,
+        body: `👨‍🎓 Don't forget to study!`
+    }
+}
+
+export function setLocalNotification() {
+    AsyncStorage.getItem(FLASHCARDS_NOTIFICATION_DB).then(JSON.parse).then((data) => {
+        if (data === null) {
+            Permissions.AsyncStorage(Permissions.NOTIFICATIONS).then(({ status }) => {
+                if (status === 'granted') {
+                    Notifications.cancelAllScheduledNotificationsAsync()
+                    // Create a new data
+                    const tomorrow = new Date()
+                    // Every day we will be receiving a notification if the user has permitted us to do do
+                    tomorrow.setDate(tomorrow.getDate() + 1)
+                    tomorrow.setHours(20)
+                    tomorrow.setMinutes(0)
+                    Notifications.scheduleLocalNotificationAsync(createLocalNotification(), {
+                        time: tomorrow,
+                        repeat: 'day'
+                    })
+                    AsyncStorage.setItem(FLASHCARDS_NOTIFICATION_DB, JSON.stringify(true))
+                }
+            })
+        }
+    })
+}
+
+export function clearLocalNotification() {
+    return AsyncStorage.removeItem(FLASHCARDS_NOTIFICATION_DB).then(Notifications.cancelAllScheduledNotificationsAsync)
+}
